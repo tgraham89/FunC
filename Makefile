d1fddb7e88b21380a914c4b5081ccb42a78d189f
Brendan <jbf2173@columbia.edu>
2024-03-19 13:18:32 -0400

    adding these changes so that the scanner build properly

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
 
-
 scanner: scanner.mll
 	ocamlbuild scanner.native
 
--- ast.ml
+++ ast.ml
@@ -9,7 +9,6 @@ type bop = Add
           | Mult
           | Div
 
-
 type expr =
   Literal of int
   | BoolLit of bool
@@ -20,38 +19,39 @@ type expr =
   | Binop of expr * bop * expr
   | Assign of string * expr
   | ListLit of expr list
-  (* | ExprList of expr list *)
-
-
-type bind = Decl of typ * string 
-            | Defn of typ * string * expr
-            (* | Struct of string * bind list *)
+  | Struct of expr list
+  | Function of bind list * stmt list
+  | Zero
 and
-(*type*) typ = Int 
-          | Bool 
-          | String 
-          | Char 
-          | Void
-          | Float 
-          | List of typ
-          | Struct of string * bind list
-
-
-type stmt =
+bind = Decl of typ * string 
+            | Defn of typ * string * expr
+and
+typ = 
+  Int 
+  | Bool 
+  | String 
+  | Char 
+  | Void
+  | Float 
+  | List of typ
+  | StructSig of string * bind list
+  | FunSig of typ list * typ
+and
+stmt =
   | Block of stmt list
   | Expr of expr
   | Bind of bind 
   | If of expr * stmt * stmt
   | While of expr * stmt
   | For of bind * expr * expr * stmt
-
-type program = {
+and
+program = {
   body: stmt list;
 }
 
 (* Pretty-printing functions *)
 let string_of_op = function
-    Add -> "+"
+  Add -> "+"
   | Sub -> "-"
   | Equal -> "=="
   | Neq -> "!="
@@ -63,58 +63,58 @@ let string_of_op = function
   | Div -> "/"
 
 let rec string_of_expr = function
-    Literal(l) -> string_of_int l
+  Zero -> "0"
+  | Literal(l) -> string_of_int l
   | BoolLit(true) -> "true"
   | BoolLit(false) -> "false"
   | StrLit(s) -> s
   | ChrLit(c) -> String.make 1 c
   | FloatLit(f) -> string_of_float f
-  | ListLit(lst) -> let rec help = function 
-      [] -> "" 
-      | x :: [] -> string_of_expr x
-      | x :: xs -> string_of_expr x ^ ", "
-      in "[" ^ (help lst) ^ "]"
+  | ListLit(lst) -> "[" ^ string_of_expr_list ", " lst ^ "]" 
+  | Struct(lst) -> "{" ^ string_of_expr_list ", " lst ^ "}" 
   | Id(s) -> s
-  | Binop(e1, o, e2) ->
-    string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+  | Binop(e1, o, e2) -> string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
   | Assign(v, e) -> v ^ " = " ^ string_of_expr e
-
-
-let rec string_of_typ = function
-    Int -> "int"
+  | Function(args, body) -> "(" ^ string_of_bind_list ", " args ^ ") {\n\t" ^ string_of_stmt_list ";\n\t" body ^ "}"
+and
+string_of_expr_list delim = function
+  [] -> ""
+  | x :: rest -> string_of_expr x ^ delim ^ string_of_expr_list delim rest
+and
+string_of_typ = function
+  Int -> "int"
   | Bool -> "bool"
   | Char -> "char"
   | String -> "string"
   | Void -> "void"
   | Float -> "float"
   | List t -> "list<" ^ (string_of_typ t) ^ ">"
-  | Struct (name, fields) -> let rec help = function
-                                [] -> ""
-                                | x :: rest -> string_of_bind (x :: []) ^ ";\n" ^ help rest in
-                                "struct " ^ name ^ " {\n" ^ help fields ^ "}"
+  | StructSig (name, fields) -> "struct " ^ name ^ " {\n" ^ string_of_bind_list ";\n" fields ^ "}\n"
+  | FunSig (args, ret) -> "function<" ^ string_of_typ_list ", " args ^ "> -> " ^ string_of_typ ret 
+and
+string_of_typ_list delim = function
+  [] -> ""
+  | x :: rest -> string_of_typ x ^ delim ^ string_of_typ_list delim rest
 and
 string_of_bind = function
-    [] -> "\n"
-    | Defn(t, id, value) :: [] -> (string_of_typ t) ^ " " ^ id ^ " = " ^ (string_of_expr value)
-    | Decl(t, id) :: [] -> (string_of_typ t) ^ " " ^ id
-    (* | Struct(name, fields) :: [] -> "struct " ^ name ^ " {\n" ^ string_of_bind fields ^ "}" *)
-    | x :: rest -> "\t" ^ string_of_bind (x :: []) ^ ";\n" ^ string_of_bind rest
-
-
-let rec string_of_stmt = function
-    Block(stmts) ->
-    "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+    Defn(t, id, value) -> string_of_typ t ^ " " ^ id ^ " = " ^ string_of_expr value
+    | Decl(t, id) -> string_of_typ t ^ " " ^ id
+and
+string_of_bind_list delim = function
+  [] -> ""
+  | x :: rest -> string_of_bind x ^ delim ^ string_of_bind_list delim rest
+and
+string_of_stmt = function
+  Block(stmts) -> "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
   | Expr(expr) -> string_of_expr expr ^ ";\n";
-  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
-                      string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
-  | Bind(bnd) -> string_of_bind (bnd::[])
+  | If(e, s1, s2) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
+  | Bind(bnd) -> string_of_bind bnd
   | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
-  | For(i, e1, e2, s) -> "for (" ^ string_of_bind (i::[]) ^ " " ^ (string_of_expr e1) ^ " " ^ (string_of_expr) e2 ^ ")" ^ string_of_stmt s
-
-(* let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n" *)
-
+  | For(i, e1, e2, s) -> "for (" ^ string_of_bind i ^ " " ^ string_of_expr e1 ^ " " ^ string_of_expr e2 ^ ")" ^ string_of_stmt s
+and string_of_stmt_list delim = function
+  [] -> ""
+  | x :: rest -> delim ^ string_of_stmt x ^ string_of_stmt_list delim rest
 let string_of_program fdecl =
   "\n\nParsed program: \n\n" ^
-  (* String.concat "" (List.map string_of_vdecl fdecl.locals) ^ *)
   String.concat "" (List.map string_of_stmt fdecl.body) ^
   "\n"
