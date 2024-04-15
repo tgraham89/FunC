6df1a7f01dec314533bf2de63cd138d9f7443d7c
Syed Raza <syedmuhammadraza@Syeds-MacBook-Pro-2.local>
2024-04-14 21:17:51 -0400

    Changes to irgen.ml

--- src/example.mc
+++ src/example.mc
@@ -1,24 +1,6 @@
-/* The GCD algorithm in FunC */
-int a;
-int b;
-
-/*
-function<int,int> -> int gcd = (a, b) => {
-  while (a != b) {
-    if (b < a) a = a - b;
-    else b = b - a;
-  }
-  return a;
-}
-
 function<> -> int main = () => {
-  int x;
-  int y;
-  a = 18;
-  b = 9;
-  x = 2;
-  y = 14;
-  return 0;
-}
+    print("hello, world");
+    return 0;
+};
 
-*/
+main();
--- src/irgen.ml
+++ src/irgen.ml
@@ -12,4 +12,17 @@ let translate (program) =
      we will generate code *)
   let the_module = L.create_module context "FunC" in
 
+  (* Get types from the context *)
+  let i32_t      = L.i32_type    context
+  and i8_t       = L.i8_type     context
+  and i1_t       = L.i1_type     context in
+
+  (* Return the LLVM type for a MicroC type *)
+  let ltype_of_typ = function
+      A.Int   -> i32_t
+    | A.Bool  -> i1_t
+  in
+
+  
+
   the_module
