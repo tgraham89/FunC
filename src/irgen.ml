1ae8e31dc6ef06882a387119599f9d8172e6680e
tgraham89 <thomashgraham@gmail.com>
2024-04-23 10:47:12 -0400

    Added print functionality

--- src/irgen.ml
+++ src/irgen.ml
@@ -12,7 +12,8 @@ let translate ({sbody} as program) =
 
   let i32_t      = L.i32_type    context
   and i8_t       = L.i8_type     context
-  and i1_t       = L.i1_type     context in
+  and i1_t       = L.i1_type     context
+  and void_t     = L.void_type   context in
 
   (* Types *)
   let ltype_of_typ = function
@@ -38,15 +39,40 @@ let translate ({sbody} as program) =
     in
     List.fold_left add_global StringMap.empty stmts
   in
-  let global_vars = init_global_vars sbody in
+  let global_vars = init_global_vars sbody in 
 
-   (* Define the main function *)
+   (* Define the print function *)
+   let print_func =
+    let printf_ty = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
+    L.declare_function "printf" printf_ty the_module in
+
+
+    (* Define the main function *)
   let main_func = 
     let main_ty = L.function_type i32_t [||] in
     L.define_function "main" main_ty the_module in
   let builder = L.builder_at_end context (L.entry_block main_func) in
-  
-  (* Add other code here *)
+
+      (* Define a function to print a string *)
+  let print_string_fn = 
+    let print_str_ty = L.function_type void_t [| L.pointer_type i8_t |] in
+    L.define_function "print_string" print_str_ty the_module in
+  let print_string_builder = L.builder_at_end context (L.entry_block print_string_fn) in
+  let str_arg = L.param print_string_fn 0 in
+  let print_format_str = L.build_global_stringptr "%s\n" "fmt" print_string_builder in
+  ignore(L.build_call print_func [| print_format_str; str_arg |] "" print_string_builder);
+  ignore(L.build_ret_void print_string_builder);
+
+  (* Example: parsing print statements *)
+  let rec gen_stmt builder = function
+    (* | SFunction (x, y) ->
+        let str_val = L.build_global_stringptr "Hello World" "str" builder in
+        ignore(L.build_call print_string_fn [| str_val |] "" builder) *)
+    (* Handle other types of statements as necessary *)
+    | _ -> let str_val = L.build_global_stringptr "Hello World" "str" builder in
+        ignore(L.build_call print_string_fn [| str_val |] "" builder)
+  in
+  List.iter (gen_stmt builder) sbody;
   
   (* Add a return statement to the main function *)
   ignore(L.build_ret (L.const_int i32_t 0) builder);
